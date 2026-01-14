8a710cc648adf87ef64dd7e60e90743f5d8c24be
Akas_Datta <012akasdatta@gmail.com>
2026-01-14 21:45:50 +0600

    added bmr calculation

--- bmi.js
+++ bmi.js
@@ -4,7 +4,20 @@ const gender = prompt("Enter your gender (male/female):");
 const age = parseFloat(prompt("Enter your age:"));
 const activity = parseFloat(prompt("Enter your activity level (sedentary, light, moderate, active, very-active):"));
 
+
 // Now BMI calculation
 const bmi = weight / (height * height);
 console.log(`Your BMI is: ${bmi}`);
 
+
+
+// BMR Calculation 
+let bmr;
+
+if( gender === "male"){
+    bmr = 10 * weight + 6.25 * (height * 100) - 5 * age + 5;
+}
+else {
+    bmr = 10 * weight + 6.25 * (height * 100) - 5 * age - 161;
+}
+console.log(`Your BMR is: ${bmr}`);
