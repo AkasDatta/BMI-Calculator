b1f1b28520e382d22479772c80b8efd84d87c73b
Akas_Datta <012akasdatta@gmail.com>
2026-01-14 21:41:39 +0600

    added bmi

--- bmi.js
+++ bmi.js
@@ -2,4 +2,9 @@ const height = parseFloat(prompt("Enter your height in meters:"));
 const weight = parseFloat(prompt("Enter your weight in kilograms:"));
 const gender = prompt("Enter your gender (male/female):");
 const age = parseFloat(prompt("Enter your age:"));
-const activity = parseFloat(prompt("Enter your activity level (Sedentary, ):"));
+const activity = parseFloat(prompt("Enter your activity level (sedentary, light, moderate, active, very-active):"));
+
+// Now BMI calculation
+const bmi = weight / (height * height);
+console.log(`Your BMI is: ${bmi}`);
+
