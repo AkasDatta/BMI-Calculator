640b4850e77bcf0b74fe227345a0afeaa84fd23e
Akas_Datta <012akasdatta@gmail.com>
2026-01-14 21:37:23 +0600

    added values like height, weight, gender and other

--- bmi.js
+++ bmi.js
@@ -1,1 +1,5 @@
 const height = parseFloat(prompt("Enter your height in meters:"));
+const weight = parseFloat(prompt("Enter your weight in kilograms:"));
+const gender = prompt("Enter your gender (male/female):");
+const age = parseFloat(prompt("Enter your age:"));
+const activity = parseFloat(prompt("Enter your activity level (Sedentary, ):"));
